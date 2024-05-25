af018ab4ffdd1d4018643571284a42090bf0dae0
Arlinda Brahimi <46766925+brahimiarlinda@users.noreply.github.com>
2024-05-25 10:51:07 +0200

    Updated the scenarios

--- sample_test.ts
+++ sample_test.ts
@@ -5,7 +5,7 @@ const baseUrl = 'https://games.lotto24.de';
 
 Feature('sample');
 
-Scenario('I validate navigation to a page',  async () => {
+Scenario('I can navigate from the game page, to the help page and back to the game page',  async () => {
     I.say(`I am on the landing page ${baseUrl}`);
     I.amOnPage('/');
 
@@ -46,3 +46,51 @@ Scenario('I validate navigation to a page',  async () => {
     I.seeCurrentUrlEquals(`${baseUrl}/games/hamburgheist`);
     I.see('Hamburg Heist');
 });
+
+Scenario('I can navigate from the game page to the help page',  async () => {
+    I.say(`I am on the landing page ${baseUrl}`);
+    I.amOnPage('/');
+
+    I.say('I accept the cookies')
+    I.click(Cookies.acceptAllCookiesButton);
+
+    I.say('I scroll to the instant games and click on the third one');
+    I.scrollTo(LandingPage.instantGamesSection);
+    I.click(LandingPage.thirdInstantGame);
+
+    I.say('I verify that the correct help link “Zur Hamburg Heist Hilfe” is on the page');
+    I.see('Zur Hamburg Heist Hilfe');
+
+    I.say('I click on the help link');
+    I.click(GamePage.helpLink);
+
+    I.say('I verify that the help page of the respective game is opened');
+    I.seeCurrentUrlEquals(`${baseUrl}/hilfe/games/hamburgheist`);
+    I.see('Hilfe');
+    I.see('Hamburg Heist');
+
+    I.say('I verify that the correct game is selected in the combo box on the help page');
+    const selectedGame = await I.grabValueFrom(HelpPage.selectedGameInDropdown);
+    I.assertEqual(selectedGame, "gahamburgheist", "The select box should have gahamburgheist selected!");
+});
+
+Scenario('I can navigate from the help page back to the respective game',  async () => {
+    I.say(`I am on the landing page ${baseUrl}`);
+    I.amOnPage('/');
+
+    I.say('I accept the cookies')
+    I.click(Cookies.acceptAllCookiesButton);
+
+    I.say('I scroll to the instant games and click on the third one');
+    I.scrollTo(LandingPage.instantGamesSection);
+    I.click(LandingPage.thirdInstantGame);
+
+    I.say('I click on the help link');
+    I.click(GamePage.helpLink);
+    
+    I.say('I verify that the button on the help page links back to the correct game');
+    I.click(HelpPage.backToGameButton);
+    I.seeCurrentUrlEquals(`${baseUrl}/games/hamburgheist`);
+    I.see('Hamburg Heist');
+});
+
